db18d73752603c539ca098ed929cc17fb33fef3b
Daniel Serleg <daniel.serleg@bitrise.io>
2022-05-03 15:43:07 +0200

    Deprecate the package

--- crypto/aes_256_gcm.go
+++ crypto/aes_256_gcm.go
@@ -8,12 +8,14 @@ import (
 )
 
 // AES256GCMCipher ...
+// Deprecated: We don't support this function and this library anymore, please use the `bitrise-io/bits` package at https://github.com/bitrise-io/bits
 func AES256GCMCipher(textToEncrypt string, iv []byte, encryptKey string) ([]byte, error) {
 	data := []byte(textToEncrypt)
 	return AES256GCMCipherBytesInput(data, iv, []byte(encryptKey))
 }
 
 // AES256GCMCipherBytesInput ...
+// Deprecated: We don't support this function and this library anymore, please use the `bitrise-io/bits` package at https://github.com/bitrise-io/bits
 func AES256GCMCipherBytesInput(data []byte, iv []byte, encryptKey []byte) ([]byte, error) {
 	block, err := aes.NewCipher(encryptKey)
 	if err != nil {
@@ -31,6 +33,7 @@ func AES256GCMCipherBytesInput(data []byte, iv []byte, encryptKey []byte) ([]byt
 }
 
 // AES256GCMDecipher ...
+// Deprecated: We don't support this function and this library anymore, please use the `bitrise-io/bits` package at https://github.com/bitrise-io/bits
 func AES256GCMDecipher(encryptedText, iv []byte, encryptKey string) (string, error) {
 	plaintext, err := AES256GCMDecipherByteOutput(encryptedText, iv, []byte(encryptKey))
 	if err != nil {
@@ -41,6 +44,7 @@ func AES256GCMDecipher(encryptedText, iv []byte, encryptKey string) (string, err
 }
 
 // AES256GCMDecipherByteOutput ...
+// Deprecated: We don't support this function and this library anymore, please use the `bitrise-io/bits` package at https://github.com/bitrise-io/bits
 func AES256GCMDecipherByteOutput(encryptedText, iv []byte, encryptKey []byte) ([]byte, error) {
 	block, err := aes.NewCipher(encryptKey)
 	if err != nil {
--- crypto/iv_generator.go
+++ crypto/iv_generator.go
@@ -3,6 +3,7 @@ package crypto
 import "github.com/pkg/errors"
 
 // GenerateIV ...
+// Deprecated: We don't support this function and this library anymore, please use the `bitrise-io/bits` package at https://github.com/bitrise-io/bits
 func GenerateIV() ([]byte, error) {
 	secureRandomBytes, err := SecureRandomBytes(12)
 	if err != nil {
